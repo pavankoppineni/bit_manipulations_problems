36aeb44d1df9d97f1cc3443c1fbf3c43c12e4c3a
Koppineni, Pavan K. (Tax&Accounting) <pavan.koppineni@thomsonreuters.com>
2020-10-18 12:41:11 +0530

    Added mask all bits from MSB to ith bit

---
+++ src/BitManipulationProblems/MaskAllBitsFromMostSignificantBit/MaskAllBitsFromMostSignificantBitV1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitManipulationProblems.ToggleAllBitsFromMostSignificantBit
+{
+    /// <summary>
+    /// https://www.geeksforgeeks.org/bit-tricks-competitive-programming/
+    /// </summary>
+    public class MaskAllBitsFromMostSignificantBitV1
+    {
+        public int MaskAllBitsFromMostSignificantBit(int number, int index)
+        {
+            var mask = (1 << index) - 1;
+            return number = number & mask;
+        }
+    }
+}
---
+++ test/BitManipulationProblems.Tests/MaskAllBitsFromMostSignificantBit/MaskAllBitsFromMostSignificantBitV1Tests.cs
@@ -0,0 +1,28 @@
+﻿using BitManipulationProblems.ToggleAllBitsFromMostSignificantBit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitManipulationProblems.Tests.MaskAllBitsFromMostSignificantBit
+{
+    [TestClass]
+    public class MaskAllBitsFromMostSignificantBitV1Tests
+    {
+        [TestMethod]
+        public void Given_NumberAndIndexToMaxFromMSB_When_MaskAllBitsFromMostSignificantBit_Then_ShouldMaskAllBitsFromMSBToIndex()
+        {
+            //Given
+            var number = 108;
+            var index = 4;
+            var problem = new MaskAllBitsFromMostSignificantBitV1();
+            var expectedResult = 12;
+
+            //When
+            var actualResult = problem.MaskAllBitsFromMostSignificantBit(number, index);
+
+            //Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
